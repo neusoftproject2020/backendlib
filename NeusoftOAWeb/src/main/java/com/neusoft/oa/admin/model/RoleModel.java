ce6604deba289b0cd903a58a344b4b62bde58f35
吕海东 <haidonglu@126.com>
2020-07-27 11:16:20 +0800

    吕海东:管理员模块增加的角色RoleModel类。

---
+++ NeusoftOAWeb/src/main/java/com/neusoft/oa/admin/model/RoleModel.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.neusoft.oa.admin.model;
+
+import java.io.Serializable;
+
+/**
+ * @author 吕海东
+ * 人事模块的角色Model类
+ */
+public class RoleModel implements Serializable {
+
+}
